ad17c1efa4e715b3f49efa0e99c9bbe0c10b098b
Jonathan Pang <66697656+pangj130@users.noreply.github.com>
2021-03-06 12:47:16 -0500

    test problem 12

--- Problem12.swift
+++ Problem12.swift
@@ -5,3 +5,18 @@ There exists a staircase with N steps, and you can climb up either 1 or 2 steps 
 Given N, write a function that returns the number of unique ways you can climb the staircase. 
 The order of the steps matters.
 */
+
+func Solution(_ n: Int) -> Int {
+    if n == 0 {
+        return 0
+    }
+    if n == 1 {
+        return 1
+    }
+    return Solution(n - 2) + Solution(n - 1)
+}
+
+print(Solution(3))
+/*
+3 -> (2, 1) -> ((1, 0), 0)
+*/
